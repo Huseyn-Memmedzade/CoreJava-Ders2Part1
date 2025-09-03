3f7700363eed97bd6be89cb11a7d1f323788e1dc
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-09-03 12:26:36 +0400

    Add files via upload

---
+++ TelebeHaqqindaTest.java
@@ -0,0 +1,21 @@
+package yenipabka;
+
+public class TelebeHaqqindaTest {
+
+    public static void main(String[] args) {
+
+
+        String name = "Nigar";
+        int age = 20;
+        double bal = 85.5;
+        boolean kurs = true;
+
+
+        System.out.println(" Tələbənin adı: " + name);
+        System.out.println(" Yaşı: " + age);
+        System.out.println(" Ortalama balı: " + bal);
+        System.out.println(" Birinci kursdurmu?: " + kurs);
+    }
+
+
+}
